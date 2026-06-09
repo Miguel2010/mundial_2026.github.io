d5ec61e4b69c9bf7e8b692d44d96769936fca7ca
Miguel García Serrano <djmiwy@gmail.com>
2026-06-09 13:55:03 +0200

    Update LeaderboardTable.tsx

--- src/features/leaderboard/LeaderboardTable.tsx
+++ src/features/leaderboard/LeaderboardTable.tsx
@@ -40,6 +40,7 @@ export function LeaderboardTable({ rows }: LeaderboardTableProps) {
           <tbody>
             {rows.map((row, index) => (
               <tr key={`${row.participante}-${index}`} className={getRowClassName(index)}>
+                {/*<td>{index + 1}</td> para no leer la posición del fichero*/}
                 <td>{row.posicion}</td>
                 <td className="player-name">{row.participante}</td>
                 <td>{row.grupos}</td>
