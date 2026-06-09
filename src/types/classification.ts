db8a8706a9c20e2a64952b1935e1c8b87d35ee64
Miguel García Serrano <djmiwy@gmail.com>
2026-06-09 13:14:01 +0200

    Update classification.ts

--- src/types/classification.ts
+++ src/types/classification.ts
@@ -1,12 +1,13 @@
 export type ClassificationCsvRow = {
-  nombre: string;
+  posicion: number;
+  participante: string;
   grupos: number;
   dieciseisavos: number;
   octavos: number;
   cuartos: number;
   semifinales: number;
-  final: number;
   tercerCuarto: number;
+  final: number;
 };
 
 export type ClassificationRow = ClassificationCsvRow & {
